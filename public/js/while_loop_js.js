7151bcded2d4f5024e6bb52936bf8a1b0fe8c732
Craig Re <Mac@Craigs-MacBook-Pro.local>
2015-10-05 11:23:39 -0500

    Added For Loop files

--- public/js/while_loop_js.js
+++ public/js/while_loop_js.js
@@ -1,6 +1,26 @@
-var i = 2;
+'use strict';
 
-while (i < 70000) {
-    console.log('while loop iteration #' + i);
-    i = i * 2;
-}
+    var i = 2;
+
+    while (i < 70000) {
+        console.log('while loop iteration #' + i);
+        i *= 2;
+// or
+//      i *= 2;
+//      console.log(i);
+   }
+
+
+
+// Just another way to write the While_loop
+
+//var i = 1;
+
+//while(i <= 16) {
+//    console.log( Math.pow(2, i) );
+ //   i += 1;  
+ // or
+ //   i = i + 1;
+ // or
+  //  i++;
+//}
