d737e3902b84bf9d9b5041cdac1ab300931f99f3
Craig Re <car.code240z@gmail.com>
2015-11-05 10:54:35 -0600

    update foreach_books.php

---
+++ php/foreach_books.php
@@ -0,0 +1,39 @@
+<?php
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+$books = array(
+    array('title' => 'The Hobbit', 'published' => 1937, 'author' => 'J. R. R. Tolkien', 'pages' => 310),
+    array('title' => 'Game of Thrones', 'published' => 1996, 'author' => 'George R. R. Martin', 'pages' => 835),
+    array('title' => 'The Catcher in the Rye', 'published' => 1951, 'author' => 'J. D. Salinger', 'pages' => 220),
+    array('title' => 'A Tale of Two Cities', 'published' => 1859, 'author' => 'Charles Dickens', 'pages' => 544),
+);
+
+foreach ($books as $book1) {
+    foreach ($book1 as $key => $value) {
+        echo "Book $key $value" . PHP_EOL;
+    }
+    echo PHP_EOL;
+// ['title']}, {$book['published']}, {$book['author']}, {$book['pages']}." 
+}
