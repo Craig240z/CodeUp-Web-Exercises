5d1fa52ec3d969214b4ce1753cda67719603405b
Craig Re <car.code240z@gmail.com>
2015-11-05 09:50:23 -0600

    more foreach practice

--- php/foreach.php
+++ php/foreach.php
@@ -2,24 +2,27 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-for ($i = 0; $i < count($things); $i++) {
-    if (is_array($things[$i])) {
-        for ($b = 0; $b < count($things[$i]); $b++) {
-            echo "{$things[$i][$b]}, ";
-        }
-            echo PHP_EOL;
+foreach ($things as $items) {
+    echo "\$things has a value of {$items}" . PHP_EOL;
+}
+// for ($i = 0; $i < count($things); $i++) {
+    // if (is_array($things[$i])) {
+        // for ($b = 0; $b < count($things[$i]); $b++) {
+            // echo "{$things[$i][$b]}, " . PHP_EOL;
+        // }
+            // echo PHP_EOL;
 
-    } else if (is_scalar($things[$i])) {
-        echo "{$things[$i]}" . PHP_EOL;
+    // } else if (is_scalar($things[$i])) {
+        // echo "{$things[$i]}" . PHP_EOL;
     // } else if (is_bool($things[$i])) {
         // echo "{$things[$i]}" . PHP_EOL;
     // } else if (is_float($things[$i])) {
         // echo "{$things[$i]}" . PHP_EOL;
     // } else if (is_integer($things[$i])) {
         // echo "{$things[$i]}" . PHP_EOL;
-    } else if (is_null($things[$i])) {
-        echo "{$things[$i]}" . PHP_EOL;
+    // } else if (is_null($things[$i])) {
+        // echo "{$things[$i]}" . PHP_EOL;
     // } else if (is_string($things[$i])) {
         // echo "{$things[$i]}" . PHP_EOL;
-    }
-}
+    // }
+// }
