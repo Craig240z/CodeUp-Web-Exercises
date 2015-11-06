29466375002046cedc14e68319d04ae42aedd820
Craig Re <car.code240z@gmail.com>
2015-11-06 13:01:54 -0600

    update arithmetic

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -3,43 +3,64 @@
 $a = 10;
 $b = 2;
 
-function add($a, $b) {
+function areValidInputs($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
+        return true;    
+    } 
+}
+function errorMessages($a, $b) {
+    return "ERROR: Both arguments must be numbers. You gave {$a} and {$b}." . PHP_EOL;
+}
+function isZero($b) {
+    if ($b == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function add($a, $b) {
+    if (areValidInputs($a, $b))  {
         return $a + $b;
     }  else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+        return errorMessages($a, $b) ;
     }
 }
 
 function subtract($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (areValidInputs($a, $b))  {
         return $a - $b;// Add code here
     } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+        return errorMessages($a, $b);
     }
 }
 
 function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (areValidInputs($a, $b)) {
         return $a * $b;// Add code here
     } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+        return errorMessages($a, $b);
     }
 }
 
 function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b) && ($a) != 0 && ($b) !=0) {
-        return $a / $b;// Add code here
+    if (areValidInputs($a, $b)) {
+        if (isZero($b)) {
+            return "ERROR: Cannot divide by zero. Got {$b}." . PHP_EOL;
+        } 
+        return ($a / $b);
     } else {
-        return "ERROR: Both arguments must be numbers and or Zero is not divisible. Try again." . PHP_EOL;
+        return errorMessages($a, $b);
     }
 }
-
+       
 function modulus($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a % $b;
-    } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+    if (areValidInputs($a, $b)) {
+        if (isZero($b)) {
+            return "ERROR: Cannot modulus by zero . Try again. Got {$b}." . PHP_EOL;
+        }
+        return ($a % $b);
+     } else {
+            return errorMessages($a, $b); 
     }
 }
 
@@ -54,11 +75,11 @@ function modulus($a, $b) {
 //         return 'error';
 //     }
 // }
-echo add(5, 4) . PHP_EOL;
+echo add(a, 4) . PHP_EOL;
 echo subtract(5, 4) . PHP_EOL;
 echo multiply(5, 4) . PHP_EOL;
-echo divide(5, 4) . PHP_EOL;   
-echo modulus(5, 4) . PHP_EOL;
+echo divide(5, 0) . PHP_EOL;   
+echo modulus(5, 0) . PHP_EOL;
 
 echo add(10, 2) . PHP_EOL;
 echo subtract(10, 2) . PHP_EOL;
