ae0b07b324139e0b60eeda5f7a451f3eca686a2e
Craig Re <car.code240z@gmail.com>
2015-11-06 09:36:56 -0600

    update arithmetic.php

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,32 +1,40 @@
 <?php
 
+$a = 10;
+$b = 2;
+
 function add($a, $b)
 {
     return $a + $b;
 }
 echo add(5, 4) . PHP_EOL;
+echo add(10, 2) . PHP_EOL;
 
 function subtract($a, $b)
 {
     return $a - $b;// Add code here
 }
 echo subtract(5, 4) . PHP_EOL;
+echo subtract(10, 2) . PHP_EOL;
 
 function multiply($a, $b)
 {
     return $a * $b;// Add code here
 }
 echo multiply(5, 4) . PHP_EOL;
+echo multiply(10, 2) . PHP_EOL;
 
 function divide($a, $b)
 {
     return $a / $b;// Add code here
 }
 echo divide(5, 4) . PHP_EOL;   
-
+echo divide(10, 2) . PHP_EOL;
 function modulus($a, $b) 
 {
     return $a % $b;
 }
 echo modulus(5, 4) . PHP_EOL;
+echo modulus(10, 2) . PHP_EOL;
+
 // Add code to test your functions here
