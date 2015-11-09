3f87d3551fb4592553b3594669068eb72d9dc885
Craig Re <car.code240z@gmail.com>
2015-11-09 14:53:34 -0600

    update list_with_commas.php

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,2 +1,7 @@
 <?php
 
+$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$physicistsArray = explode(', ', $famousFakePhysicists);
+
+var_dump($physicistsArray);
