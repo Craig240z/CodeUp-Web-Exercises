fef52b2af22053319bafdbff8003cd776131f54d
Craig Re <car.code240z@gmail.com>
2015-11-09 15:56:13 -0600

    update list_with_commas.php

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,12 +1,36 @@
 <?php
 
-$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, and Tony Stark';
+$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 $physicistsArray = explode(', ', $famousFakePhysicists);
 
-print_r($physicistsArray);
+$name = 'and ' . array_pop($physicistsArray);
+
+array_push($physicistsArray, $name);
 
 $famousFakePhysicists = implode(', ', $physicistsArray);
 
+print_r($physicistsArray);
+
+
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+
+function humanizedList($physicistsArray) {
+
+}
+   // Your solution goes here!
+
+ // List of famous peeps
+ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+ // TODO: Convert the string into an array
+ $physicistsArray = [];
+
+ // Humanize that list
+ $famousFakePhysicists = humanizedList($physicistsArray);
+
+ // Output sentence
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+
+ ?>
