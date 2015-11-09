a3f46c4210f97a585256f5ec4e7a5719d272570f
Craig Re <car.code240z@gmail.com>
2015-11-09 15:12:57 -0600

    update list_with_commas.php

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,7 +1,12 @@
 <?php
 
-$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, and Tony Stark';
 
 $physicistsArray = explode(', ', $famousFakePhysicists);
 
-var_dump($physicistsArray);
+print_r($physicistsArray);
+
+$famousFakePhysicists = implode(', ', $physicistsArray);
+
+
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
