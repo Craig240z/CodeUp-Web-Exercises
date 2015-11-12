e971ece6a8d8188b516887bf7a631fdd1a752643
Craig Re <car.code240z@gmail.com>
2015-11-12 10:19:51 -0600

    made changes, still working out solution to shift the kruft off the top of the array.

--- php/csv_parse.php
+++ php/csv_parse.php
@@ -1,12 +1,55 @@
 <?php
 
-function parseSalesReport($filename);
-
+function parseSalesReport($filename)
+{
     $salesReportArray = array();
+    $handle = fopen($filename, 'r');
+    $contentString = fread($handle, filesize($filename));
+    $contentString = trim($contentString);
+
+    $arrayOfStrings = explode("\n", $contentString);
+    $arrayOfStrings = trimTheKruft($arrayOfStrings);
+
+    
+    // var_dump($arrayOfStrings);
+    function trimTheKruft($arrayOfStrings) {
+        for($i = 0; $1 <= 0; $i++)
+    }
+    $kruft[0 - 12] = array_shift($arrayOfStrings);
+    print_r($arrayOfStrings);
+
+        foreach($salesReportArray as $row) {
+    
+    $columns = explode(',', $row);
+    
+    foreach($columns as $key => $cell) {
+         echo $cell;
+    }
+}
+sprintf(format)
+// $stack = array("orange", "banana", "apple", "raspberry");
+// $fruit = array_shift($stack);
+// print_r($stack);
+//         // foreach ($arrayOfStrings as $personContactInfoString) {
+        //     $personInfoArray = explode('|', $personContactInfoString);
 
 
+// $contentsArray = [
+//     "bob, 2222222, bob@bob.com, 600 Navarro",
+//     "jane, 3333333, jane@jane.com, 112 E. Pecan"
+//     "JJ, 4444444, JJ@jj.com, 110 E. Houston"
+// ];
 
 
+// foreach($contentsArray as $row) {
+    
+//     $columns = explode(',', $row);
+    
+//     foreach($columns as $key => $cell) {
+//          echo $cell;
+//     }
+// }
 
 
-    var_dump(parseContacts('../data/report.txt'));
+}
+    var_dump(parseSalesReport('../data/report.txt'));
