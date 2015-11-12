86032e8ea4fd0b70af5aa67993a27dbd3897e63e
Craig Re <car.code240z@gmail.com>
2015-11-12 10:50:11 -0600

    fleshing out the first function for writing to a log file.

--- php/logger.php
+++ php/logger.php
@@ -2,8 +2,17 @@
 
 function logMessage($logLevel, $message)
 {
-    // todo - complete this function
-}
+    $handle = fopen($filename, 'a');
+        $todayDate = date("Y-M-D");
+        $filename = '..data/log-' . $today  . '.log';
+        $todayTime = $todayDate . '' . time("H:M:S");
+
+
+// foreach ($newCities as $city) {
+//     fwrite($handle, PHP_EOL . $city);
+// }
+// fclose($handle);// todo - complete this function
+// }
 
 logMessage("INFO", "This is an info message.");
 logMessage("ERROR", "This is an info message.");
