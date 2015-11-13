c16addeb025f0f36e5c0a870f24b805fd24f77ad
Craig Re <car.code240z@gmail.com>
2015-11-13 14:07:39 -0600

    Added function pageController.

--- public/counter.php
+++ public/counter.php
@@ -1,20 +1,22 @@
 <?php 
 
-
-
-
-
-
-
-
-
+function pageController()
+{
+    $counter = isset($_GET['count'])?$_GET['count']: 0;
+    return array(
+    'counter' => $counter
+    );
+}
+extract(pageController());
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Counter</title>
 </head>
 <body>
-
+    <h2><?=$counter?></h2>
+    <a href="counter.php?count=<?=$counter+1?>">Up</a>
+    <a href="counter.php?count=<?=$counter-1?>">Down</a>
 </body>
 </html>
