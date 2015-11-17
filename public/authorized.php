ea1a28653b6afc21accb3b86bb07f86ceb0b284e
Craig Re <car.code240z@gmail.com>
2015-11-17 10:29:53 -0600

    Connected logout.php to authorized and added endsession code.

--- public/authorized.php
+++ public/authorized.php
@@ -1,15 +1,23 @@
 <?php 
+session_start();
 
+// get the current session id
+$sessionId = session_id();
 
-
+if($_SESSION['LOGGED_IN_USER'] == FALSE) {
+        header('location: login.php');
+        die();
+   } 
+   $userName = $_SESSION['userName'];
 ?>
-(!DOCTYPE html)
+<!DOCTYPE html>
 <html>
 <head>
     <title>Authorized</title>
 </head>
 <body>
-    <h2>Authorized</h2>
-
+     <h2>You are an authorized user.</h2>
+    <h2>You are logged in as <?= $userName ?></h2>
+    <a href="logout.php">Logout</a>
 </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -3,34 +3,38 @@
 // var_dump($_POST);
 
 $message = '';
+// Creating a session 
+session_start();
+// get the current session id
+$sessionId = session_id();
+$userName = '';
+$_SESSION['LOGGED_IN_USER'] = false;
+
 
 if(isset($_POST['userName']) && isset($_POST['password'])) {
-    // Creating a session 
-    session_start();
 
-    // get the current session id
-    $sessionId = session_id();
 
     $userName =  htmlspecialchars(strip_tags($_POST['userName']));
     $password =  htmlspecialchars(strip_tags($_POST['password']));
 
     // Creating logged_in_user if not created already.
-    if(!isset($_SESSION['LOGGED_IN_USER'])) {
-        $_SESSION['LOGGED_IN_USER'] = $userName;
+    if(isset($_SESSION['LOGGED_IN_USER'])) {
+        $_SESSION['LOGGED_IN_USER'] = TRUE;
+        $_SESSION['userName'] = $userName;
     }
-
+    // Verifying userename and password.
     if($userName == 'guest' && $password == 'password') {
-        // header('Location: authorized.php');
+        header('Location: authorized.php');
+        // Good message.
         $message = "Welcome " . $_SESSION['LOGGED_IN_USER'];
-        // die();
+        die();
     } else {
+        // Bad message
         $message = "Please input a valid username and password";
     }
 }
-
-
-
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
